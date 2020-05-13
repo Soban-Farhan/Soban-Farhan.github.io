75595a3407ce232f2ad57a61aeb7640c5667441d
Soban Farhan <soban.farhan@dcmail.ca>
2020-05-13 08:13:14 -0400

    Whole Website

---
+++ contactForm.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $to = "sobanfarhan@gmail.com";
+    $subject = $_POST['subject'];
+    $txt = "Sent From " . $_POST['name'] . "\n\n" . $_POST['message'];
+    $headers = "From: " . $_POST['email'];
+
+    if ( mail($to,$subject,$txt,$headers) ) {
+        echo "<img style='width: 20px;' src='./images/checkmark.png' alt='Cross Icon'> Sent Successfully.";
+    } else {
+        echo "Something Went Wrong.";
+    }
+    
+?>
